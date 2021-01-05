69d3c8d8b61834c3ff3e1b5bc291a9060d52522f
Yeongbin Kim <ybin77@kaist.ac.kr>
2021-01-05 16:50:02 +0900

    bugfix

--- app/src/main/java/com/example/basicapplicationfunction/MainActivity.java
+++ app/src/main/java/com/example/basicapplicationfunction/MainActivity.java
@@ -156,61 +156,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
         super.onActivityResult(requestCode, resultCode, resultIntent);
-        if(requestCode == Code.requestCode && resultCode == Code.resultCode){
-            /*new Thread(){
-            @Override
-            public void run() {*/
 
-            ArrayList<ContentProviderOperation> list = new ArrayList<>();
-            try{
-                list.add(
-                        ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
-                                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
-                                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
-                                .build()
-                );
-
-                list.add(
-                        ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-
-                                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-                                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, resultIntent.getStringExtra("name"))   //이름
-
-                                .build()
-                );
-
-                list.add(
-                        ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-
-                                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, resultIntent.getStringExtra("number"))           //전화번호
-                                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE  , ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)   //번호타입(Type_Mobile : 모바일)
-
-                                .build()
-                );
-
-                list.add(
-                        ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-
-                                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
-                                .withValue(ContactsContract.CommonDataKinds.Email.DATA  , resultIntent.getStringExtra("email"))  //이메일
-                                .withValue(ContactsContract.CommonDataKinds.Email.TYPE  , ContactsContract.CommonDataKinds.Email.TYPE_WORK)     //이메일타입(Type_Work : 직장)
-
-                                .build()
-                );
-
-                getApplicationContext().getContentResolver().applyBatch(ContactsContract.AUTHORITY, list);  //주소록추가
-                list.clear();   //리스트 초기화
-                fragment1.refresh();
-            }catch(RemoteException e){
-                e.printStackTrace();
-            }catch(OperationApplicationException e){
-                e.printStackTrace();
-            }
-        }
     }
 
     @TargetApi(Build.VERSION_CODES.N)
